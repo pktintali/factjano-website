36ff3f8256a609c360565f0843e67d86f7641c43
pktintali <pradeepkumar8@deloitte.com>
2023-12-11 21:12:29 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'factjano-public-images.s3.ap-south-1.amazonaws.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'factjano.com',
+          },
+        ],
+      },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
